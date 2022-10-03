371fe3217eef9ce4eb14464806cf1fda3afd7b4b
Steven <stevengf14@gmail.com>
2022-10-03 16:08:53 -0500

    Test and code alert for server error response

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Options from "./Options";
+import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
